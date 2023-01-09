983d3cdc3eaa7ddf7150171cc99d87e18fcbda1c
negeek <negeek5ive@gmail.com>
2023-01-09 15:28:59 +0100

    square factors

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ A python package that gives the mathematical factors of a number fast.
 ## Example
 A quick example. In py_factors you can get your answer in unordered form and ordered form.
 
-``` # for unordered factors 
-    from py_factors import unordered_factors, ordered_factors
+``` 
+    # for unordered factors 
+    from py_factors import factors
 
+    factor200= factors(200, ordered=False)
+
+    output: [1, 200, 2, 100, 4, 50, 5, 40, 8, 25, 10, 20]
+   
+
+    #for ordered factors, ordered argument is True by default
     factor200= unordered_factors(200)
 
-    #for ordered factors
-    factor200= unordered_factors(200)
+    output: [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 200]
+
 
 ```
 
--- py_factors/factors.py
+++ py_factors/factors.py
@@ -1,33 +1,91 @@
 class Factors:
     def __init__(self):
-        self.unorderedFactors=[]
-        self.orderedFactors=[]
+        self.mathFactors=[]
+        self.squareFactors=[]
+        self.oddFactors=[]
+        self.evenFactors=[]
 
-    def unordered_factors(self, number):
-        for num in range(1, int(number**0.5)+1):
-            if number %num==0:
-                    self.unorderedFactors.append(num)
-                    if number//num != self.unorderedFactors[-1]:
-                        self.unorderedFactors.append(number//num)
+    def factors(self, number, ordered=True):
+        # if ordered output
+        if ordered:
+            otherFactors=[]  #numbers that represent number/factor
+
+            for num in range(1, int(number**0.5)+1):
+                if number %num==0:
+                        self.mathFactors.append(num)
+                        if number//num != self.mathFactors[-1]:
+                            otherFactors.append(number//num)
+                        else:
+                            break
+
+            # update factors list
+            while otherFactors!=[]:
+                self.mathFactors.append(otherFactors.pop())
+            return self.mathFactors 
+        
+        # if unordered output
+        else:
+            for num in range(1, int(number**0.5)+1):
+                if number %num==0:
+                        self.mathFactors.append(num)
+                        if number//num != self.mathFactors[-1]:
+                            self.mathFactors.append(number//num)
+                        else:
+                            return self.mathFactors
+            return self.mathFactors
+
+
+    def square_factors(self, number, ordered=True):
+        # if ordered output
+        if ordered:
+            otherFactors=[]
+            for num in range(1, int(number**0.5)+1):
+                int_root= int(num**0.5)
+                if number %num==0:
+                    #if it is a perfect square
+                    if num**0.5==int_root:
+                        self.squareFactors.append(num)
+ 
+                        #check if the other factor is a perfect square
+                        int_root=int((number//num)**0.5)
+                        if number//num != self.squareFactors[-1] and (number//num)**0.5==int_root:
+                            otherFactors.append(number//num)
                     else:
-                        return self.unorderedFactors
-        return self.unorderedFactors
+                        #check if the other factor is a perfect square
+                        int_root= int((number//num)**0.5)
+                        if number//num != self.squareFactors[-1] and (number//num)**0.5==int_root:
+                            otherFactors.append(number//num)
+                            
+            # update square factors list
+            while otherFactors!=[]:
+                self.squareFactors.append(otherFactors.pop())
+            return self.squareFactors
 
-    def ordered_factors(self, number):
-        right=[]  #numbers that represent number/factor
+        # if unorderd output
+        else:
+            for num in range(1, int(number**0.5)+1):
+                int_root= int(num**0.5)
+                if number%num==0:
+                    # if it is a perfect square
+                    if num**0.5==int_root:
+                        self.squareFactors.append(num)
 
-        for num in range(1, int(number**0.5)+1):
-            if number %num==0:
-                    self.orderedFactors.append(num)
-                    if number//num != self.orderedFactors[-1]:
-                        right.append(number//num)
+                        #check if the other factor is a perfect square
+                        int_root= int((number//num)**0.5)
+                        if number//num != self.squareFactors[-1] and (number//num)**0.5==int_root:
+                            self.squareFactors.append(number//num)
+                       
                     else:
-                        break
+                        #check if the other factor is a perfect square
+                        int_root= int((number//num)**0.5)
+                        if number//num != self.squareFactors[-1] and (number//num)**0.5==int_root:
+                            self.squareFactors.append(number//num)
+            return self.squareFactors
 
-        #append the numbers in right to ordered list
-        while right!=[]:
-            self.orderedFactors.append(right.pop())
-        return self.orderedFactors
+
+
+
+
 
 
 
